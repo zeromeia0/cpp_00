a0c688dc81c5ceb43da4b1c925c4d348cfa2aa85
Seu Nome <jeje>
2025-12-02 23:20:09 +0000

    im tired, gonna finish ex00 later

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,7 +6,26 @@
 /*   By: jeslin-ticiane <jeslinticianevaz@gmail.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 21:32:53 by jeslin-tici       #+#    #+#             */
-/*   Updated: 2025/12/02 21:32:54 by jeslin-tici      ###   ########.fr       */
+/*   Updated: 2025/12/02 23:16:20 by jeslin-tici      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <iostream>
+# include <iomanip>
+
+class Fixed{
+	private:
+		int fixed_number;
+		static const int bits = 8;
+	
+	public:
+		Fixed();
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+		~Fixed();
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+};
+#endif
